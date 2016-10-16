ed6008cc0c9cbca2253d4538616b2c4241d73b3c
Lerif <ellogador@gmail.com>
2016-10-16 01:43:45 -0200

    changed Interface

---
+++ Documents/workspace/ClinicaFisioterapia/src/ValueObject/OpcoesMenu.java
@@ -0,0 +1,9 @@
+package ValueObject;
+
+public class OpcoesMenu {
+	
+	public static final String CADASTRAR_NOVO_FUNCIONARIO = "Cadastrar funcionario";
+	public static final String CRIAR_NOVA_TERAPIA = "Criar Nova Terapia";
+	public static final String VOLTAR = "Voltar";
+
+}
